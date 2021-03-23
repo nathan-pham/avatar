4de3c96de353c728655275ccb3fa1bb3ec210d57
Nathan Pham <nathanpham.me@gmail.com>
2021-03-23 09:17:07 -0700

    web camera

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,24 @@
-import * as faceapi from "./web_modules/face-api.min.js"
+import * as faceapi from "./face-api/index.min.js"
+
+const root = document.getElementById("root")
+
+const initializeVideo = () => {
+    const video = document.createElement("video")
+    const attributes = {
+        id: "video",
+        width: 720,
+        height: 560,
+        autoplay: true
+    }
+    Object.keys(attributes).forEach(prop => video.setAttribute(prop, attributes[prop]))
+
+    root.appendChild(video)
+
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+initializeVideo()
