98e0087ba4c4efbc94321250c1fcc84042f9bc82
Nathan Pham <nathanpham.me@gmail.com>
2021-03-23 13:30:27 -0700

    detection

--- static/js/index.js
+++ static/js/index.js
@@ -1,24 +1,47 @@
-import * as faceapi from "./face-api/index.min.js"
-
 const root = document.getElementById("root")
+const modelPath = "/static/js/face-api/models"
+const resolution = {
+    width: 720,
+    height: 560
+}
+
+const applyAttributes = (element, options) => (
+    Object.keys(resolution).forEach(prop => element.setAttribute(prop, options[prop]))
+)
 
 const initializeVideo = () => {
-    const video = document.createElement("video")
-    const attributes = {
-        id: "video",
-        width: 720,
-        height: 560,
-        autoplay: true
-    }
-    Object.keys(attributes).forEach(prop => video.setAttribute(prop, attributes[prop]))
-
-    root.appendChild(video)
+    const video = root.querySelector("video")
+    applyAttributes(video, resolution)
 
     navigator.getUserMedia(
         { video: {} },
         stream => video.srcObject = stream,
         err => console.error(err)
     )
+
+    video.addEventListener("play", () => {
+        const canvas = faceapi.createCanvasFromMedia(video)
+        const context = canvas.getContext("2d")
+        applyAttributes(canvas, resolution)
+        root.appendChild(canvas)
+        
+        const animation = (async function render() {
+            const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+            const resized = faceapi.resizeResults(detections, resolution)
+            context.clearRect(0, 0, canvas.width, canvas.height)
+            faceapi.draw.drawDetections(canvas, resized)
+            faceapi.draw.drawFaceLandmarks(canvas, resized)
+            faceapi.draw.drawFaceExpressions(canvas, resized)
+            return requestAnimationFrame(render)
+        })()
+    })
+
+    return video
 }
 
-initializeVideo()
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri(modelPath),
+    faceapi.nets.faceLandmark68Net.loadFromUri(modelPath),
+    faceapi.nets.faceExpressionNet.loadFromUri(modelPath),
+    faceapi.nets.faceRecognitionNet.loadFromUri(modelPath),
+]).then(initializeVideo)
