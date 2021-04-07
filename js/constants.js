31ee04782bf64d2d56cd5afe6fcdc81152016def
Nathan Pham <nathanpham.me@gmail.com>
2021-04-06 18:18:23 -0700

    rotations

--- js/constants.js
+++ js/constants.js
@@ -4,3 +4,7 @@ const resolution = {
     width: 640,
     height: 360
 }
+const X_ROTATIONAL_SCALE = 3
+
+Math.radians = (degrees) => degrees * Math.PI / 180
+Math.degrees = (radians) => radians * 180 / Math.PI
--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ const initializeVideo = () => {
         
         // applyAttributes(canvas, resolution)
         
-        const animation = (async function render() {
+        window.animation = (async function render() {
             const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions({
                 inputSize: 160,
                 scoreThreshold: 0.45
@@ -44,20 +44,89 @@ const initializeVideo = () => {
     return video
 }
 
-const updateCube = (results) => {
-    const box = results?.detection?._box
-
-    if(!box) {
-        return
-    }
-    
+const changePosition = (box) => {
     const xPos = (box.left + box.width / 2)
     const xPercent = xPos / 640
-
+    
     const yPos = (box.top + box.height / 2)
     const yPercent = yPos / 360
 
-    TweenMax.to(cube.position, 0.2, {x: (xPercent - 0.5) * 10, y: (yPercent - 0.5) * -10, ease: Linear.easeNone});
+    return {
+        x: (xPercent - 0.5) * 10, 
+        y: (yPercent - 0.5) * -10
+    }
+}
+
+const distance = (x1, y1, x2, y2) => Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
+
+const formatPoints = (data) => {
+    const points = []
+    for(const [key, value] of Object.entries(data)) {
+        points.push([value._x, value._y])
+    }
+    return points
+}
+
+const allowNegativeIndex = (arr) => new Proxy(arr, {
+    get(target, prop) {
+        if (!isNaN(prop)) {
+            prop = parseInt(prop, 10)
+            if (prop < 0) {
+                prop += target.length
+            }
+        }
+
+        return target[prop]
+    }
+})
+
+const updateCube = (results) => {
+    const detection = results?.detection
+
+    if(!detection) {
+        return
+    }
+
+    const box = detection._box
+    const points = formatPoints(results.landmarks._positions)
+
+    Object.assign(cube.position, changePosition(box))
+
+
+    const chin = points.slice(0, 17)
+    const lEyebrow = allowNegativeIndex(points.slice(17, 22))
+    const rEyebrow = allowNegativeIndex(points.slice(22, 27))
+    const nose = points.slice(27, 36)
+    const lEye = points.slice(36, 42)
+    const rEye = points.slice(42, 48)
+    const mouth = points.slice(48) 
+
+    const eyeDistance = distance(rEye[2][0], rEye[2][1], lEye[1][0], lEye[1][1])
+    const rotationalZ = Math.atan2(rEye[2][1] - lEye[1][1], rEye[2][0] - lEye[1][0])
+
+    const lEyebrowLength = distance(lEyebrow[-1][0], lEyebrow[-1][1], lEyebrow[0][0], lEyebrow[0][1]) / eyeDistance
+    const rEyebrowLength = distance(rEyebrow[-1][0], rEyebrow[-1][1], rEyebrow[0][0], rEyebrow[0][1]) / eyeDistance
+    const eyebrowImplications = lEyebrowLength - rEyebrowLength
+    const rotationalY = Math.radians(eyebrowImplications * 90) * 0.85
+
+    const noseLength = nose[6][1] - nose[3][1]
+    // const py = 
+
+    // sum(pos_histories[1]) / len(pos_histories[1])
+    const rotationalX = Math.radians(Math.degrees((-Math.atan2(noseLength / eyeDistance, 0.05) + Math.radians(66))) * X_ROTATIONAL_SCALE) + Math.abs(rotationalY) 
+    
+    Object.assign(cube.rotation, {
+        x: rotationalX,
+        y: rotationalY,
+        z: rotationalZ
+    })
+    // * py
+
+    /*
+
+    rot_x = math.radians(math.degrees((-math.atan2(nose_length / eye_dis, 0.05) + math.radians(66))) * X_ROT_SCALE) + abs(rot_y) * py
+    main_font.render('rotx: ' + str(round(math.degrees(rot_x), 2)), screen, (4, 270))
+    */
 }
 
 Promise.all([
